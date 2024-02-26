9d5edbba30723d07aed7fec307b3b2d40d3d9507
Eric Yu <ericyu@Erics-MacBook-Pro.local>
2024-02-25 16:22:07 -0800

    changed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,18 +11,11 @@ export default function Header() {
           className="sm:w-9 sm:h-9 w-8 h-8"
         />
         <h1 className="sm:text-3xl text-2xl font-bold ml-2 tracking-tight">
-          twitterbio.io
-        </h1>
+Flashify        
+
+</h1>
       </Link>
-      <a
-        className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100"
-        href="https://github.com/Nutlope/twitterbio"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Github />
-        <p>Star on GitHub</p>
-      </a>
+     
     </header>
   );
 }
